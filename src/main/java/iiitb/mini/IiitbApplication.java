1c2ff775d8a550ea89aa863bfe203561ef22af52
hrushikesh <hrushikeshnakka@gmail.com>
2024-11-22 19:41:21 +0530

    first commit

---
+++ src/main/java/iiitb/mini/IiitbApplication.java
@@ -0,0 +1,13 @@
+package iiitb.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiitbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiitbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iiitb/mini/IiitbApplicationTests.java
@@ -0,0 +1,13 @@
+package iiitb.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiitbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
